b41251d592fb240c26df4b8da6b8c1b3ee15dd4f
jutta2555 <jutta25022555@gmail.com>
2021-11-24 14:39:38 +0700

    main

--- run.py
+++ run.py
@@ -12,5 +12,8 @@ if __name__ == "__main__":
     app = create_app()
     app.config['DEBUG'] = True
     app.config['MONGO_URI'] = config['PROD']['DB_URI']
+    # 'mongodb+srv://mflixuser:CQNQmXIQXlnqD4fQ@sandbox.hkmmq.mongodb.net/sample_mflix'
+    # 
+    # 
 
     app.run()
